d2b643352b14469f5df83c816338abab03c8e0e6
Chi-Cheong Weng <chicheongweng@gmail.com>
2015-04-24 18:16:10 +0800

    wrapped comment

--- Competition_TextData.R
+++ Competition_TextData.R
@@ -1,11 +1,11 @@
 # KAGGLE COMPETITION - DEALING WITH THE TEXT DATA
-
-# This script file is intended to help you deal with the text data provided in the competition data files
-
-# If you haven't already, start by reading the data into R
-# Make sure you have downloaded these files from the Kaggle website, and have navigated to the directory where you saved the files on your computer 
+# This script file is intended to help you deal with the text data provided in the competition data 
+# files. If you haven't already, start by reading the data into R
+# Make sure you have downloaded these files from the Kaggle website,
+# and have navigated to the directory where you saved the files on your computer 
 
 # We are adding in the argument stringsAsFactors=FALSE, since we have some text fields
+
 set.seed(1000)
 NewsTrain = read.csv("NYTimesBlogTrain.csv", stringsAsFactors=FALSE)
 NewsTest = read.csv("NYTimesBlogTest.csv", stringsAsFactors=FALSE)
@@ -30,9 +30,6 @@ CorpusHeadline = Corpus(VectorSource(c(NewsTrain$Snippet, NewsTest$Snippet)))
 # You can go through all of the standard pre-processing steps like we did in Unit 5:
 
 CorpusHeadline = tm_map(CorpusHeadline, tolower)
-
-# Remember this extra line is needed after running the tolower step:
-
 CorpusHeadline = tm_map(CorpusHeadline, PlainTextDocument)
 CorpusHeadline = tm_map(CorpusHeadline, removePunctuation)
 CorpusHeadline = tm_map(CorpusHeadline, removeWords, stopwords("english"))
@@ -46,24 +43,31 @@ sparse = removeSparseTerms(dtm, 0.98)
 HeadlineWords = as.data.frame(as.matrix(sparse))
 
 # Let's make sure our variable names are okay for R:
-
 colnames(HeadlineWords) = make.names(colnames(HeadlineWords))
 
 # Now we need to split the observations back into the training set and testing set.
 # To do this, we can use the head and tail functions in R. 
-# The head function takes the first "n" rows of HeadlineWords (the first argument to the head function), where "n" is specified by the second argument to the head function. 
-# So here we are taking the first nrow(NewsTrain) observations from HeadlineWords, and putting them in a new data frame called "HeadlineWordsTrain"
+# The head function takes the first "n" rows of HeadlineWords 
+# (the first argument to the head function), where "n" is specified by the second 
+# argument to the head function. 
+# So here we are taking the first nrow(NewsTrain) observations from HeadlineWords, 
+# and putting them in a new data frame called "HeadlineWordsTrain"
 
 HeadlineWordsTrain = head(HeadlineWords, nrow(NewsTrain))
 
-# The tail function takes the last "n" rows of HeadlineWords (the first argument to the tail function), where "n" is specified by the second argument to the tail function. 
-# So here we are taking the last nrow(NewsTest) observations from HeadlineWords, and putting them in a new data frame called "HeadlineWordsTest"
+# The tail function takes the last "n" rows of HeadlineWords (the first argument 
+# to the tail function), where "n" is specified by the second argument to the tail 
+# function. So here we are taking the last nrow(NewsTest) observations from 
+# HeadlineWords, and putting them in a new data frame called "HeadlineWordsTest"
 
 HeadlineWordsTest = tail(HeadlineWords, nrow(NewsTest))
 
-# Note that this split of HeadlineWords works to properly put the observations back into the training and testing sets, because of how we combined them together when we first made our corpus.
-
-# Before building models, we want to add back the original variables from our datasets. We'll add back the dependent variable to the training set, and the WordCount variable to both datasets. You might want to add back more variables to use in your model - we'll leave this up to you!
+# Note that this split of HeadlineWords works to properly put the observations back 
+# into the training and testing sets, because of how we combined them together when 
+# we first made our corpus. Before building models, we want to add back the original
+# variables from our datasets. We'll add back the dependent variable to the training
+# set, and the WordCount variable to both datasets. You might want to add back more
+# variables to use in your model - we'll leave this up to you!
 
 HeadlineWordsTrain$Popular = NewsTrain$Popular
 HeadlineWordsTrain$WordCount = NewsTrain$WordCount
@@ -74,9 +78,8 @@ HeadlineWordsTest$WordCount = NewsTest$WordCount
 HeadlineWordsTest$NewsDesk = NewsTest$NewsDesk
 HeadlineWordsTest$SectionName = NewsTest$SectionName
 
-# Remember that you can always look at the structure of these data frames to understand what we have created
-
-
+# Remember that you can always look at the structure of these data frames to 
+# understand what we have created
 # Now let's create a logistic regression model using all of the variables:
 
 #HeadlineWordsLog = glm(Popular ~ ., data=HeadlineWordsTrain, family=binomial)
