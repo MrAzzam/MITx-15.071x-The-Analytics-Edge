6ba3a92e213e1c8cd0d2d692084da67280443397
Chi-Cheong Weng <chicheongweng@gmail.com>
2015-04-24 18:42:53 +0800

    split HeadlineWordsTest and HeadlineWordsTrain

--- Competition_TextData.R
+++ Competition_TextData.R
@@ -25,7 +25,7 @@ library(tm)
 # dataset for text analytics, but we will just show you how to use this particular 
 # variable. Note that we are creating a corpus out of the training and testing data.
 
-CorpusHeadline = Corpus(VectorSource(c(NewsTrain$Snippet, NewsTest$Snippet)))
+CorpusHeadline = Corpus(VectorSource(c(NewsTrain$Snippet)))
 
 # You can go through all of the standard pre-processing steps like we did in Unit 5:
 
@@ -60,7 +60,41 @@ HeadlineWordsTrain = head(HeadlineWords, nrow(NewsTrain))
 # function. So here we are taking the last nrow(NewsTest) observations from 
 # HeadlineWords, and putting them in a new data frame called "HeadlineWordsTest"
 
+
+# --------------
+  
+
+CorpusHeadline = Corpus(VectorSource(c(NewsTest$Snippet)))
+
+# You can go through all of the standard pre-processing steps like we did in Unit 5:
+
+CorpusHeadline = tm_map(CorpusHeadline, tolower)
+CorpusHeadline = tm_map(CorpusHeadline, PlainTextDocument)
+CorpusHeadline = tm_map(CorpusHeadline, removePunctuation)
+CorpusHeadline = tm_map(CorpusHeadline, removeWords, stopwords("english"))
+CorpusHeadline = tm_map(CorpusHeadline, stemDocument)
+
+# Now we are ready to convert our corpus to a DocumentTermMatrix, remove sparse terms, and turn it into a data frame. 
+# We selected one particular threshold to remove sparse terms, but remember that you can try different numbers!
+
+dtm = DocumentTermMatrix(CorpusHeadline)
+sparse = removeSparseTerms(dtm, 0.98)
+HeadlineWords = as.data.frame(as.matrix(sparse))
+
+# Let's make sure our variable names are okay for R:
+colnames(HeadlineWords) = make.names(colnames(HeadlineWords))
+
+# Now we need to split the observations back into the training set and testing set.
+# To do this, we can use the head and tail functions in R. 
+# The head function takes the first "n" rows of HeadlineWords 
+# (the first argument to the head function), where "n" is specified by the second 
+# argument to the head function. 
+# So here we are taking the first nrow(NewsTrain) observations from HeadlineWords, 
+# and putting them in a new data frame called "HeadlineWordsTrain"
+
 HeadlineWordsTest = tail(HeadlineWords, nrow(NewsTest))
+
+# -------------
 
 # Note that this split of HeadlineWords works to properly put the observations back 
 # into the training and testing sets, because of how we combined them together when 
